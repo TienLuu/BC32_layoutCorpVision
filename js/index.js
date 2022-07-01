d30174f29ff72b469d5c2f8bb1960564e29aa000
TienLuu <tienluu1301@gmail.com>
2022-07-01 11:46:23 +0700

    Update Source

---
+++ js/index.js
@@ -0,0 +1,15 @@
+/*
+    INIT SLICK
+*/
+
+// Carousel
+$(document).ready(function () {
+   $(".carousel__content").slick({
+      autoplay: true,
+      fade: true,
+      loop: true,
+      pauseOnFocus: false,
+      pauseOnHover: false,
+      speed: 2000,
+   });
+});
